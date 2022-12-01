ee729762f2d7f56df35932d229d648e7a558c052
0liverloo <116297666+oliverloo123@users.noreply.github.com>
2022-12-01 12:06:04 -0500

    Delete ejercicio directory

--- ejercicio/semana13-1.py
+++
@@ -1,8 +0,0 @@
-def centro(cad):
-    print(" " * int(40 - (len(cad)/2)),cad)
-    print(" " * int(40 - (len(cad)/2)), "=" * len(cad))
-
-mensaje1 = "Un mensaje centrado"
-centro(mensaje1)
-mensaje2 = "Otro mensaje"
-centro(mensaje2)
--- ejercicio/semana13-8.py
+++
@@ -1,8 +0,0 @@
-def calculo_factorial(num):
-    if num == 1:
-        return 1
-    else:
-        return num * calculo_factorial(num-1)
-
-numero1 = int(input("Numero: "))
-print("El factorial es: ", calculo_factorial(numero1))
